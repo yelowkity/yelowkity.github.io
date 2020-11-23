7ea836b384c20647ba686a0c99de4bdb6572fcce
yelowkity <71228352+yelowkity@users.noreply.github.com>
2020-11-22 19:17:30 -0500

    Update lab8script.js

--- lab_8/public/js/lab8script.js
+++ lab_8/public/js/lab8script.js
@@ -24,10 +24,10 @@ async function getData() {
                 */
     			let randomIndex = Math.floor(Math.random() * listSize);
               	let item = json.data.children[randomIndex];
-              	let subreddit = item.subreddit;
-              	let author = item.author_fullname;
-              	let title = item.title;
-              	let ups = item.ups;
+              	let subreddit = item.data.subreddit;
+              	let author = item.data.author_fullname;
+              	let title = item.data.title;
+              	let ups = item.data.ups;
 				let message = "<b>Subreddit </b>: " + subreddit + " <b>Author</b>:" + author + " <b>Title</b>:" + title + " <b>Up votes</b>: " + ups;
               	let li = document.createElement("li");
               	li.innerHTML = message;
